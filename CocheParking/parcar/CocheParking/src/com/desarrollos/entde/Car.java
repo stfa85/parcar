36aa9be11e0fa5b18f4f2a98b440dd5ff5331123
stfa85 <94706586+stfa85@users.noreply.github.com>
2021-12-21 22:08:59 +0100

    Corregido

---
+++ CocheParking/parcar/CocheParking/src/com/desarrollos/entde/Car.java
@@ -0,0 +1,90 @@
+package com.desarrollos.entde;
+
+/**
+ * Class that models objects of type "car"
+ * 
+ * @Java MOOC team, UC3M
+ * @2.0
+ */
+public class Car
+{
+    // Attributes of the class Car
+    static int numberWheels = 4;                // Number of wheels                           
+    
+    Garage theGarage=null;
+
+    
+    /**
+     * Constructor of a car specifying the garage object
+     */
+    public Car (Garage g){
+    	theGarage = g;
+    }
+    /**
+     * Method that is being always invoked when the program is running
+     * @throws InterruptedException 
+     */
+    public static void main(String[] args) throws InterruptedException{
+    	Garage g = new Garage();
+    	Car car = new Car(g);
+        Car.accelerate();
+        slowDown();
+        moveDown();
+        accelerate();
+        car.printGarage();
+//        turnRight();
+//        turnHeadlightsOn();
+//        turnHeadlightsOff();
+    }    
+
+    private void printGarage() {
+		// TODO Auto-generated method stub
+		
+	}
+	/**
+     * Accelerate the car
+     * @throws InterruptedException 
+     */
+    public static void accelerate() throws InterruptedException{
+        String c = new String("*");
+        String space = new String(" ");
+    	for(int i=0; i<5; i++)
+        {
+        	System.out.print(c);
+        	c = space.concat(c);
+        	Thread.sleep(1000);
+        }
+    }
+    
+    /**
+     * Slow down the car
+     * @throws InterruptedException 
+     */
+    public static void slowDown() throws InterruptedException{
+    	String c = new String("     *");
+    	for(int i=0; i<5; i++)
+        {
+        	System.out.print(c);
+        	c = c.substring(1);
+        	Thread.sleep(1000);
+        }
+    	System.out.println();
+    }    
+    
+    /**
+     * Move the car downwards
+     * @throws InterruptedException 
+     */    
+    public static void moveDown() throws InterruptedException{
+    	String c = new String("\t\t\t\t  *");
+    	for(int i=0; i<5; i++)
+        {
+    		if(i!=4)
+    			System.out.println(c);
+    		else 
+    			System.out.print(c);
+        	Thread.sleep(1000);
+        }
+    }
+    
+}
